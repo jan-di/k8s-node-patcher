e23746a5a6362ccf76a9f692f432c79bfe06c611
Jan Dittrich <jan.dittrich@cgm.com>
2023-11-16 12:12:18 +0100

    Add helm chart
    Signed-off-by: Jan Dittrich <jan.dittrich@cgm.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,15 @@ set -eu
 
 echo "Running k8s-node-patcher"
 
+apiserver_url=https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT
+serviceaccount=/var/run/secrets/kubernetes.io/serviceaccount
+apiserver_token=$(cat ${serviceaccount}/token)
+apiserver_cacert=${serviceaccount}/ca.crt
+
+mkdir -p /tmp/k8s-node-patcher
+
+curl --cacert "$apiserver_cacert" --header "Authorization: Bearer ${apiserver_token}" -X GET "${apiserver_url}/api/v1/nodes" -o /tmp/k8s-node-patcher/node.json
+
 ansible --version
 
 # Sleep for all eternity
